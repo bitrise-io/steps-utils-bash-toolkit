5a1e0c3fb71cc751a1a649ec4ccbdb9aa0981ddf
Viktor Benei <viktor.benei@gmail.com>
2014-09-19 08:18:44 +0200

    Update utils.sh

--- utils.sh
+++ utils.sh
@@ -5,7 +5,7 @@
 #  Example: print_and_do_command echo 'hi'
 #
 function print_and_do_command {
-	echo "$ $@"
+	echo "-> $ $@"
 	$@
 }
 
@@ -18,19 +18,31 @@ function print_and_do_command {
 #  of the precedence order of the '>' operator
 #
 function print_and_do_command_string {
-	echo "$ $1"
+	echo "-> $ $1"
 	eval "$1"
 }
 
+#
+# Combination of print_and_do_command and error checking, exits if the command fails
+#  Example: print_and_do_command_exit_on_error rm some/file/path
+function print_and_do_command_exit_on_error {
+	print_and_do_command $@
+	if [ $? -ne 0 ]; then
+		echo " [!] Failed!"
+		exit 1
+	fi
+}
 
 #
-# Check the last command's result code and if it's not zero then print the given error message and exit with the command's exit code
+# Check the LAST COMMAND's result code and if it's not zero
+#  then print the given error message and exit with the command's exit code
 #
 function fail_if_cmd_error {
+	err_msg=$1
 	last_cmd_result=$?
-	if [ $last_cmd_result -ne 0 ]; then
-		echo $1
-		exit $last_cmd_result
+	if [ ${last_cmd_result} -ne 0 ]; then
+		echo "${err_msg}"
+		exit ${last_cmd_result}
 	fi
 }
 # TEST/EXAMPLE:
